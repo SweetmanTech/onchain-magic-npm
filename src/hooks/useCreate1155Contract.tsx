3230fda17e252614b0a96577f04d5411b0f56eb2
Sweets Sweetman <sweetmantech@gmail.com>
2023-12-17 11:27:51 -0500

    I switch from npm to yarn.

--- src/hooks/useCreate1155Contract.tsx
+++ src/hooks/useCreate1155Contract.tsx
@@ -1,48 +1,49 @@
-import { usePrivy } from "@privy-io/react-auth"
-import { useRouter } from "next/router"
-import { Interface } from "ethers/lib/utils"
-import abi from "../lib/abi/abi-Zora1155CreatorProxy.json"
-import dropAbi from "../lib/abi/abi-ERC1155Drop.json"
-import handleTxError from "../lib/handleTxError"
-import { useDeploy } from "../providers/DeployContext"
-import usePrivySendTransaction from "./usePrivySendTransaction"
-import { chainId } from "../lib/consts"
-import { getZoraBlob, store } from "../lib/ipfs"
-import getZora1155ProxyAddress from "../lib/zora/getZora1155ProxyAddress"
-import { getCreated1155DropsByAddress } from "../lib/zora/getCreated1155DropsByAddress"
-import { useUserProvider } from "../providers/UserProvider"
+import { usePrivy } from "@privy-io/react-auth";
+import { useRouter } from "next/router";
+import { Interface } from "ethers/lib/utils";
+import abi from "../lib/abi/abi-Zora1155CreatorProxy.json";
+import dropAbi from "../lib/abi/abi-ERC1155Drop.json";
+import handleTxError from "../lib/handleTxError";
+import { useDeploy } from "../providers/DeployContext";
+import usePrivySendTransaction from "./usePrivySendTransaction";
+import { chainId } from "../lib/consts";
+import { getZoraBlob, store } from "../lib/ipfs";
+import getZora1155ProxyAddress from "../lib/zora/getZora1155ProxyAddress";
+import { getCreated1155DropsByAddress } from "../lib/zora/getCreated1155DropsByAddress";
+import { useUserProvider } from "../providers/UserProvider";
 
 const useCreate1155Contract = () => {
-  const { direccionDePago } = useDeploy()
-  const { authenticated } = usePrivy()
-  const { sendTransaction } = usePrivySendTransaction()
-  const factoryAddress = getZora1155ProxyAddress(chainId)
-  const { connectedWallet } = useUserProvider()
-  const { push } = useRouter()
-  const contractName = "S T O R I E S 🪄"
-
-  const signTransaction = async (args) => {
-    if (authenticated) {
-      await sendTransaction(factoryAddress, chainId, abi, "createContract", args)
-      const response = await getCreated1155DropsByAddress(connectedWallet, chainId)
-      push(`/create/story/${response[0]}`)
-    }
-  }
+  const { direccionDePago } = useDeploy();
+  const { authenticated } = usePrivy();
+  const { sendTransaction } = usePrivySendTransaction();
+  const factoryAddress = getZora1155ProxyAddress(chainId);
+  const { connectedWallet } = useUserProvider();
+  const { push } = useRouter();
+  const contractName = "S T O R I E S 🪄";
 
   const createContract = async () => {
     try {
-      const ipfs = await store(getZoraBlob(direccionDePago), contractName, "", direccionDePago)
-      const adminPermissionArgs = [0, connectedWallet, 2]
-      const minterPermissionArgs = [0, process.env.NEXT_PUBLIC_FIXED_PRICE_SALE_STRATEGY, 4]
+      const ipfs = await store(
+        getZoraBlob(direccionDePago),
+        contractName,
+        "",
+        direccionDePago
+      );
+      const adminPermissionArgs = [0, connectedWallet, 2];
+      const minterPermissionArgs = [
+        0,
+        process.env.NEXT_PUBLIC_FIXED_PRICE_SALE_STRATEGY,
+        4,
+      ];
       const minterPermissionCall = new Interface(dropAbi).encodeFunctionData(
         "addPermission",
-        minterPermissionArgs,
-      )
+        minterPermissionArgs
+      );
       const adminPermissionCall = new Interface(dropAbi).encodeFunctionData(
         "addPermission",
-        adminPermissionArgs,
-      )
-      const setupActions = [adminPermissionCall, minterPermissionCall]
+        adminPermissionArgs
+      );
+      const setupActions = [adminPermissionCall, minterPermissionCall];
 
       const args = [
         `ipfs://${ipfs}`,
@@ -54,16 +55,16 @@ const useCreate1155Contract = () => {
         },
         direccionDePago,
         setupActions,
-      ]
-      signTransaction(args)
+      ];
+      signTransaction(args);
     } catch (err) {
-      handleTxError(err)
+      handleTxError(err);
     }
-  }
+  };
 
   return {
     deploy: createContract,
-  }
-}
+  };
+};
 
-export default useCreate1155Contract
+export default useCreate1155Contract;
